288cb6b97a9178f99fdcabd8201d0f4c5aff11e4
Tristan Van Veen <tajvanvbusiness@gmail.com>
2025-08-23 15:18:51 +0200

    startup of project wie eet er mee

---
+++ app.js
@@ -0,0 +1,20 @@
+gsap.registerPlugin(ScrollSmoother);
+
+window.addEventListener("load", () => {
+  //scrollsmoother
+  const smoother = ScrollSmoother.create({
+    wrapper: "#smooth-wrapper",
+    content: "#smooth-content",
+    smooth: 1.2,
+    effects: true,
+  });
+
+  //mobile menu
+  const menu = document.querySelector("#mobile-menu");
+  const menuLinks = document.querySelector(".navbar_menu");
+
+  menu.addEventListener("click", () => {
+    menu.classList.toggle("is-active");
+    menuLinks.classList.toggle("active");
+  });
+});
